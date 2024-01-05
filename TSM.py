2c19e43e9465a60c9d221fbbdf5a8bf0e846f528
LGNWJQ <WJQpersonal@163.com>
2024-01-05 21:14:46 +0800

    test commit in pycharm
    Signed-off-by: LGNWJQ <WJQpersonal@163.com>

--- TSM.py
+++ TSM.py
@@ -2,6 +2,9 @@ import torch
 from torchmetrics.image import PeakSignalNoiseRatio as PSNR
 from torchmetrics.image import LearnedPerceptualImagePatchSimilarity as LPIPS
 
+"""
+DFVDFV
+"""
 
 if __name__ == "__main__":
     a = torch.randint(5, (10,))
