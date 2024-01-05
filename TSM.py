4bdd142594884c05813d7e0a2959dbcbffe632b0
LGNWJQ <60968738+LGNWJQ@users.noreply.github.com>
2024-01-05 20:59:05 +0800

    Delete TSM.py

--- TSM.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torchmetrics.image import PeakSignalNoiseRatio as PSNR
-from torchmetrics.image import LearnedPerceptualImagePatchSimilarity as LPIPS
-
-
-if __name__ == "__main__":
-    a = torch.randint(5, (10,))
-
-    print(a)
