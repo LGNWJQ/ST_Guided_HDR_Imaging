d759b405e18e7673541d52d5271494f60786c154
LGNWJQ <WJQpersonal@163.com>
2024-01-06 20:12:21 +0800

    完善data_utils数据处理操作，未完成
    Signed-off-by: LGNWJQ <WJQpersonal@163.com>

--- dataset/data_utils.py
+++ dataset/data_utils.py
@@ -1,12 +1,64 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+import imageio
+from albumentations.pytorch import ToTensorV2
+import numpy as np
+imageio.plugins.freeimage.download()
+imageio.core.util.appdata_dir("imageio")
+
+
+def read_LDR_as_tensor(path, max_value=65535.0):
+    '''
+    :param path: 图像路径
+    :param max_value: 最大动态范围
+    :return: 四维张量
+    '''
+    ToTensor = ToTensorV2()
+    image_np = imageio.v2.imread(path).astype("float32") / max_value
+    image_t = ToTensor(image=image_np)['image']
+    return image_t[None,]
+
+def read_HDR_as_tensor(path):
+    ToTensor = ToTensorV2()
+    image_np = imageio.v2.imread(path, format='HDR-FI')
+    image_t = ToTensor(image=image_np)['image']
+    return image_t[None,]
+
+def save_tensor_as_tiff(tensor_image, path, max_value=65535.0):
+    '''
+    :param tensor_image: 四维张量，float32，[0, 1]
+    :param path: 保存路径
+    :param max_value: 最大动态范围
+    '''
+    numpy_image = tensor_image.squeeze(0).permute(1, 2, 0).detach().cpu().numpy() * max_value
+    numpy_image = numpy_image.astype(np.uint16)
+    imageio.v2.imwrite(path, numpy_image)
+
+def save_tensor_as_hdr(tensor_image, path):
+    HDR = tensor_image.squeeze(0).permute(1, 2, 0).detach().cpu().numpy()
+    imageio.v2.imwrite(path, HDR)
+
+
+# def Calculate_Mean(tensor_list):
+#     lum_list = []
+#     for image_tensor in tensor_list:
+#         pass
+
+
+def ToGray(image):
+    r, g, b = image.unbind(dim=-3)
+    gray = (0.2989 * r + 0.587 * g + 0.114 * b)
+    return gray, torch.mean(gray).detach().cpu().item()
+
 
 def Normalize_neg_one_to_one(img):
     return img * 2 - 1
 
-def Unnormalize_zero_to_one(normed_img):
+
+def UnNormalize_zero_to_one(normed_img):
     return (normed_img + 1) * 0.5
+
 
 def Gamma_Correction_HtoL(image, gamma=2.2):
     return torch.pow(image, 1 / gamma)
@@ -109,3 +161,21 @@ class Image_Gradient(nn.Module):
         grad = self.gradient(gray)
 
         return grad
+
+
+def print_info(x):
+    print('=-' * 20)
+    print('shape: ', x.shape)
+    print(f'range: [{x.min()}, {x.max()}]')
+    print('dtype:', x.dtype)
+    print('=-' * 20)
+
+
+from torchvision.utils import save_image
+if __name__ == "__main__":
+    base_path = r'D:\BASIC_FILE\DT\My_Dataset\Scenes\BabyAtWindow\Img'
+    for i in range(1, 8):
+        path = base_path + f'{i}.tif'
+        tensor_image = read_LDR_as_tensor(path).cuda()
+        gray, lum = ToGray(tensor_image)
+        print(lum)
